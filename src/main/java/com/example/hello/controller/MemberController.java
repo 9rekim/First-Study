1011fe1349f71fb9f9c5720b5efad0c08a02e25c
9rekim <9rekim@gmail.com>
2023-01-28 21:45:54 +0900

    7th Commit

--- src/main/java/com/example/hello/controller/MemberController.java
+++ src/main/java/com/example/hello/controller/MemberController.java
@@ -3,10 +3,13 @@ package com.example.hello.controller;
 import com.example.hello.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.hello.domain.Member;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -32,4 +35,12 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }

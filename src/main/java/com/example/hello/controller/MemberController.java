35bb2d70ab54557a93f2a7f5fbf530ad479a1f7d
9rekim <9rekim@gmail.com>
2023-01-28 21:31:38 +0900

    6th Commit

--- src/main/java/com/example/hello/controller/MemberController.java
+++ src/main/java/com/example/hello/controller/MemberController.java
@@ -3,6 +3,10 @@ package com.example.hello.controller;
 import com.example.hello.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.hello.domain.Member;
 
 @Controller
 public class MemberController {
@@ -14,5 +18,18 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }

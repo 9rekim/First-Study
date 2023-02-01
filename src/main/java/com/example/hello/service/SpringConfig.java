6b60ad5f50d490c21a198a466113f91121178561
9rekim <9rekim@gmail.com>
2023-02-01 21:04:26 +0900

    10th Commit

--- src/main/java/com/example/hello/service/SpringConfig.java
+++ src/main/java/com/example/hello/service/SpringConfig.java
@@ -1,20 +1,35 @@
 package com.example.hello.service;
 
+import com.example.hello.repository.JpaMemberRepository;
 import com.example.hello.repository.MemberRepository;
 import com.example.hello.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+/*    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
-    }
+    }*/
+
+/*    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository();
+    }*/
 }
